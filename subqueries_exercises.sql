960aa93ba546abd559b81de753b6ce134d6afa1a
M Coindreau <mariannacoindreau@gmail.com>
2017-10-18 15:22:40 -0500

    subqueries exercises completed

---
+++ subqueries_exercises.sql
@@ -0,0 +1,30 @@
+# 1. Find all the employees with the same hire date as employee 101010 using a sub-query.
+#
+# 69 Rows
+
+select *
+from employees where hire_date = (
+  select hire_date from employees where emp_no = 101010
+);
+
+# 2. Find all the titles held by all employees with the first name Aamod.
+#
+# 314 total titles, 6 unique titles
+
+select title
+from titles
+where emp_no in (
+  select emp_no from employees
+  where first_name = 'Aamod'
+);
+
+# 3. Find all the current department managers that are female.
+select *
+from departments
+where dept_no in (
+  select dept_no from dept_manager
+  where emp_no in (
+    select emp_no from employees where gender = 'F'
+  )
+);
+
